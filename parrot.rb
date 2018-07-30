fdc4830f79b999a02d6edade3e3e2f2a08b2697e
Caroline Berg <carolineeberg@gmail.com>
2018-07-30 16:34:09 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,5 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot(name, call = "Squawk!")
+  name = Ammar
+  puts "#{name}, my parrot, is so loud. Listen: #{call}"
+  return parrot(name, call)
+end
